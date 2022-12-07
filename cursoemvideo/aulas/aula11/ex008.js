94d02456a592ef6efa2dd85ac2a5c977fd7f7c3d
MarceloCarvalhoRJ <117359391+MarceloCarvalhoRJ@users.noreply.github.com>
2022-12-07 15:14:54 +0000

    criei exer 6,7,8 e 9.

---
+++ cursoemvideo/aulas/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel=81
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 80) {
+    console.log('Voce ultrapassou o limite de velodicade. Multado!')
+} else {
+    console.log('Velocidade dentro do permitido')
+}
+console.log('Diriga usando cinto de segurança')
