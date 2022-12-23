a09d2d0cd3fe9200c42f8daf7ec7d0d4b27c75c8
MarceloCarvalhoRJ <117359391+MarceloCarvalhoRJ@users.noreply.github.com>
2022-12-23 13:13:48 +0000

    exercícios de loop

---
+++ cursoemvideo/aulas/aula14/loop_for.js
@@ -0,0 +1,16 @@
+
+console.log("Vai começar...")
+for (var c=1; c <= 5; c++){
+    console.log(c)
+}console.log("Fim!")
+
+
+/*do {
+    console.log(c)
+    c++
+}while (c <= 5)*/
+
+/*while (c <= 5){
+    console.log(c)
+    c++
+}*/
